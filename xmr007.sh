b1f22f7916092aa8640a6e2cb7f2167d5655589a
tandewpal <106701896+tandewpal@users.noreply.github.com>
2022-06-05 11:42:06 -0500

    Create xmr007.sh

---
+++ xmr007.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear
+lscpu
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar -xf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0/
+./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 83bGWn25vGMB8PMKgPEpTzHvktfx39rG8KfhJjuKBmEH1iucJfXHTjUdsnqq5mmBuCNxTpjULARYa6GUHV1g29oYJ386xa8.xmr007 -p x
