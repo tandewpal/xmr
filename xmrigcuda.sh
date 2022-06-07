6310d837494cf651d0f63906e66819dd3837976f
tandewpal <106701896+tandewpal@users.noreply.github.com>
2022-06-07 10:00:02 +0000

    code changed cuda added

--- xmrigcuda.sh
+++ xmrigcuda.sh
@@ -4,4 +4,5 @@ lscpu
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar -xf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0/
-./xmrig --cuda -o stratum+ssl://rx.unmineable.com:443 -a rx -k -u SHIB:0xca3938760d3ff3370ca4bd5ba5f9e727dbb198e8.tanCuda003 -p x
+sysctl -w vm.nr_hugepages=1280
+./xmrig --cuda --coin=XMR -o xmr.2miners.com:2222 -u 83bGWn25vGMB8PMKgPEpTzHvktfx39rG8KfhJjuKBmEH1iucJfXHTjUdsnqq5mmBuCNxTpjULARYa6GUHV1g29oYJ386xa8.xmr001 -p x
